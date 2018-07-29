2164f206bf4e76989501a2d932c3ce8d5483dee2
Liuming Yang <ylmap@126.com>
2018-07-29 17:23:23 +0800

    save

--- main.js
+++ main.js
@@ -1,34 +1,27 @@
-
-const readline = require('readline');
+const readline = require("readline");
 
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
 });
 
+function getRandom() {
+  for (let i = 0; i < 4; i++) {
+    var randomNumber = ~~(Math.random() * 10);
+    console.log(randomNumber);
+  }
+}
 
+function main() {
+  rl.question("Please input your number:", answer => {
+    if (!isNaN(answer) && answer.length == 4) {
+      console.log(`The answer is: 0A0B`);
+    } else {
+      console.log("Wrong Input，Input again");
+    }
 
- function main(){
-    rl.question('Please input your number:', (answer) => {
-         
-         for (let i=0;i<4;i++)
-         {
-          var randomNumber=~~(Math.random()*10);
-         console.log(randomNumber);
-         }
-        
+    rl.close();
+  });
+}
 
-          if(!isNaN(answer)&&(answer.length==4))
-          {
-            console.log(`The answer is: 0A0B`);
-          }
-          else 
-          {
-            console.log('Wrong Input，Input again')
-          }
-        
-        rl.close();
-      });
- }
-
- main();
+main();
